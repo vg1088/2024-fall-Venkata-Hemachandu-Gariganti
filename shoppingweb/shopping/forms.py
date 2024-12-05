fda6fb5f986eabc11708e71f4a86324b8391cd9f
vg1088 <vg1088@usnh.edu>
2024-12-05 13:26:25 -0500

    Fixed erroe

--- shoppingweb/shopping/forms.py
+++ shoppingweb/shopping/forms.py
@@ -1,16 +1,21 @@
-# myapp/forms.py
-
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.models import User
+from .models import User
 
-class SignUpForm(UserCreationForm):
-    phone_number = forms.CharField(max_length=15, required=True)
+class SignUpForm(forms.Form):
+    username = forms.CharField(max_length=100)
+    mobile_number = forms.CharField(max_length=15)
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get("password1")
+        password2 = cleaned_data.get("password2")
 
-    class Meta:
-        model = User
-        fields = ('username', 'phone_number', 'password1', 'password2')
+        if password1 != password2:
+            raise forms.ValidationError("Passwords do not match.")
+        return cleaned_data
 
 class SignInForm(forms.Form):
-    username = forms.CharField(max_length=254, required=True)
-    password = forms.CharField(label='Password', strip=False, widget=forms.PasswordInput)
+    username = forms.CharField(max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput)
